d7ad9b34b2a16115836d15f28584b0bcb6924df0
Eric S. Stone <eric@eric-stone.com>
2017-01-22 04:17:14 +0000

    memory layout: change start address and file organization

--- setup.s
+++ setup.s
@@ -1,5 +1,18 @@
-.globl _start
+// If the ld-script, pi bootloader, and config.txt
+// are all configured correctly, this should be
+// where program-counter is initilized to after
+// kernel.img is loaded into ram.
+// We enter into 'c' code by first initializing the
+// stack-pointer, then branching to our 'sysmain' function.
+// A safety spin-loop follows just in case the sysmain
+// function ever returns (it shouldn't).
+.global _start
+
+.text
+
 _start:
-	mov sp,#0x00010000
-	bl sysmain
-loop:	b loop
+	MOV	SP, #0x100000	// 1 MiB
+	BL	sysmain
+
+spin:
+	B	spin
