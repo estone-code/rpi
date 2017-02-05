f828e86a67cab3ed727d6f5d8eab608448e8687a
Eric S. Stone <eric@eric-stone.com>
2017-02-05 21:01:49 +0000

    console: add basic printf

--- console.c
+++ console.c
@@ -1,5 +1,6 @@
 #include "aux.h"
 #include "console.h"
+#include "stdarg.h"
 #include "types.h"
 
 s32 console_init()
@@ -34,23 +35,82 @@ char console_read_char()
 	return aux_mu_rx_byte_busywait();
 }
 
-static char nibble_to_char(unsigned char c)
-{
-	if (c < 10) {
-		return c+48;
-	}
-	return c+55;
-}
-
-s32 console_write_hex(unsigned char c)
+static s32 console_write_hex(u64 c)
 {
 	unsigned char nibble, to_print;
+	int nibbles;
 
-	nibble = (c >> 4) & 0xF;
-	to_print = nibble_to_char(nibble);
-	console_write_char(to_print);
-	nibble = c & 0xF;
-	to_print = nibble_to_char(nibble);
-	console_write_char(to_print);
+	console_write("0x__");
+	for (nibbles = 16; nibbles > 0; nibbles--) {
+		nibble = (0xF000000000000000 & c) >> 60;
+		/* convert to ascii */
+		to_print = nibble + (nibble < 10 ? 48 : 55);
+		console_write_char(to_print);
+		c <<= 4;
+
+		if (nibbles == 9) {
+			console_write_char('_');
+		}
+		if (nibbles % 2 && nibbles != 1) {
+			console_write_char('_');
+		}
+	}
+	return 0;
+}
+
+/*
+ * Embarrasing but quick-to-write recursive implementation that also
+ * uses decimal division.
+ */
+static s32 console_write_decimal(s32 decimal)
+{
+	if (decimal < 0) {
+		console_write_char('-');
+		decimal *= -1;
+	}
+	if (decimal < 10) {
+		console_write_char(decimal + 48); /* convert to ascii */
+	} else {
+		console_write_decimal(decimal / 10);
+		console_write_char((decimal % 10) + 48);
+	}
+	return 0;
+}
+
+s32 console_printf(const char *format, ...)
+{
+	va_list ap;
+	const char *c;
+	u8 in_escape;
+
+	in_escape = 0;
+	va_start(ap, format);
+	for (c = format; *c; c++) {
+		if (in_escape) {
+			in_escape = 0;
+			switch (*c) {
+			case 's':
+				console_write(va_arg(ap, char *));
+				break;
+			case 'x':
+			case 'p':
+				console_write_hex(va_arg(ap, u64));
+				break;
+			case 'd':
+				console_write_decimal(va_arg(ap, s32));
+				break;
+			case '%':
+				console_write_char('%');
+				break;
+			}
+		} else {
+			if ('%' == *c) {
+				in_escape = 1;
+			} else {
+				console_write_char(*c);
+			}
+		}
+	}
+	va_end(ap);
 	return 0;
 }
--- console.h
+++ console.h
@@ -4,9 +4,9 @@
 #include "types.h"
 
 s32 console_init(void);
+s32 console_printf(const char *format, ...);
 s32 console_write(char *string);
 s32 console_write_char(char);
-s32 console_write_hex(unsigned char c);
 char console_read_char(void);
 
 #endif /* CONSOLE_H */
