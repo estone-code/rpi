177bf9570bcd9e57fc2a7d5e8fe72db904b549d2
Eric S. Stone <eric@eric-stone.com>
2017-02-17 07:26:37 +0000

    init: zero out .bss

--- sysmain.c
+++ sysmain.c
@@ -1,7 +1,22 @@
 #include "monitor.h"
+#include "types.h"
+
+/*
+ * Obtain values with '&'
+ * https://sourceware.org/binutils/docs/ld/Source-Code-Reference.html#Source-Code-Reference
+ */
+extern unsigned long _START_OF_BSS;
+extern unsigned long _END_OF_BSS;
 
 void sysmain(void)
 {
+	uintptr_t *ptr;
+
+	/* init .bss */
+	for (ptr = &_START_OF_BSS; ptr < &_END_OF_BSS; ptr++) {
+		*ptr = 0;
+	}
+
 	monitor_init();
 	while (1) {
 		monitor();
